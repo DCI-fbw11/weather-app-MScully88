96c484406287cdd7f48bd992ce2d369e3efa4809
Michael Scully <42539704+MScully88@users.noreply.github.com>
2019-01-10 13:16:13 +0100

    Weather update

--- weatherapp.js
+++ weatherapp.js
@@ -1,14 +1,22 @@
-// greeting & time
+// greeting, time & background
 
 const currentDate = new Date();
 const hours = currentDate.getHours();
-const minutes = ('0'+ currentDate.getMinutes()).slice(-2);
+const minutes = ('0' + currentDate.getMinutes()).slice(-2);
 
 let messagePrefix = "It is";
 let message = "Welcome!";
 
 document.getElementById("greeting").innerHTML = (`${message}<br>${messagePrefix} ${hours}:${minutes}`);
 
+    if (hours >= 5 && hours < 8)
+        document.body.className = "dawn";
+    else if (hours >= 8 && hours < 18)
+        document.body.className = "day";
+    else if (hours >= 18 && hours < 21)
+        document.body.className = "dusk";
+    else
+        document.body.className = "night";
 
 // retrieving data & functions
 
@@ -33,28 +41,37 @@ function enterKeyUp(e) {
     if (e.key === "Enter") {
         getWeather();
         document.getElementById("toggle").className = "show";
+        document.getElementById("degree").className = "showDegree";
+        document.getElementById("tempTxt").className = "show";
+        document.getElementById("descTxt").className = "show";
     }
 }
+
 // get weather
 
 function getWeather() {
     if (searchInput.value === "") {
-        
+        alert("Please enter a city name!")
     } else {
-        let link = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + key;
+        let link = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + key + "&units=metric";
         myHttpRequest(link, updatedData);
         document.getElementById("toggle").className = "show";
+        document.getElementById("degree").className = "showDegree";
+        document.getElementById("tempTxt").className = "show";
+        document.getElementById("descTxt").className = "show";
     }
 }
+
 // update data
 
 function updatedData(data) {
     let myData = JSON.parse(data);
     city.innerHTML = myData.name;
     icon.src = "http://openweathermap.org/img/w/" + myData.weather[0].icon + ".png";
-    temperature.innerHTML = parseInt(myData.main.temp - 273) + "&#x2103";
+    temperature.innerHTML = Math.floor(myData.main.temp);
     description.innerHTML = myData.weather[0].description;
 }
+
 // http request
 
 function myHttpRequest(url, callback) {
@@ -66,7 +83,6 @@ function myHttpRequest(url, callback) {
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
-
 
 
 
@@ -98,4 +114,3 @@ function myHttpRequest(url, callback) {
 //         else if (hours >= 18 && hours <= 23) document.getElementById("greeting").innerHTML = "Guten Abend!";
 //         break;
 // }
-
